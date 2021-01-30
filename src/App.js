fed3b9b433321dd3f48994496b26f82f8fbcb2e8
Karina <karinaa121213@gmail.com>
2021-01-30 13:51:05 +0200

    added optimized images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import {Header} from "./components/Header/Header";
+import {TopSection} from "./components/TopSection/TopSection";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <TopSection/>
     </div>
   );
 }
