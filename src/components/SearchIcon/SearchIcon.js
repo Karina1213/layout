60efe67d53c787f25bc27309fb1aee3084c58f73
Karina <karinaa121213@gmail.com>
2021-01-31 02:03:31 +0200

    added SearchIcon, ServicesSection styles updated

---
+++ src/components/SearchIcon/SearchIcon.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './SearchIcon.scss'
+
+export const SearchIcon = () => {
+
+    return (
+        <i className='search-icon fa fa-search  fa--hovered ' aria-hidden="true"> </i>
+    )
+};
