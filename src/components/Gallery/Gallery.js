e1fff19bc48fa43688dcfb11a8cfc93bad924e44
Karina <karinaa121213@gmail.com>
2021-02-01 07:25:08 +0200

    added strict equality

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -8,7 +8,7 @@ export const Gallery = () => {
     return (
         <ul className='gallery'>
             {servicesData.map((data) => {
-                const classes = (data.id == '21' || data.id == '22') ? 'gallery__card--large' : 'gallery__card--small'
+                const classes = (data.id === '21' || data.id === '22') ? 'gallery__card--large' : 'gallery__card--small'
 
                 return (
                     <li className={`gallery__card ${classes}`}>
